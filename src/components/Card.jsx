24e9713540de6dd704182f91782930cdbefeb768
isabelxklee <hi@kleetime.com>
2022-09-16 20:37:44 -0400

    added a link component to card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Card = ({ algo }) => {
   return (
     <div>
-      <h3>{algo.title}</h3>
+      <h3>
+        <Link to={`algorithms/${algo.id}`}>{algo.title}</Link>
+      </h3>
       <p>{algo.description}</p>
       <p>Difficulty: {algo.difficulty}</p>
       <p>Category: {algo.category}</p>
