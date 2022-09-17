c5bb0c19f13b7d440e3642ec9094a6a28ef2d1b8
isabelxklee <hi@kleetime.com>
2022-09-16 20:30:57 -0400

    updated error message

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -8,7 +8,9 @@ const Error = () => {
   return (
     <div id="error-page">
       <h1>Uh oh, looks this page doesn't exist!</h1>
-      <p>Error status: {error.statusText || error.message}</p>
+      <p>
+        Error status: {error.status} {error.statusText}
+      </p>
     </div>
   );
 };
