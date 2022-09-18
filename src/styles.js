683a77911b4fb7c047ed680c94d96424388c15db
isabelxklee <hi@kleetime.com>
2022-09-18 11:08:29 -0400

    updated font import

--- src/styles.js
+++ src/styles.js
@@ -1,13 +1,18 @@
 import styled, { createGlobalStyle } from "styled-components";
+import RecoletaRegular from "./fonts/Recoleta-Regular.otf";
 
 export const GlobalStyle = createGlobalStyle`
+
   @font-face {
     font-family: "Recoleta";
-    src: url("./fonts/Recoleta-Regular.otf") format("otf");
+    font-style: normal;
+    font-weight: 400;
+    src: url(${RecoletaRegular}) format("otf");
   }
 
   body {
     font-family: "Recoleta", serif;
+    font-weight: 400;
     color: #000;
     background: #fff;
   }
