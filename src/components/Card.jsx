762003efb0c25aa04248cc482901ab7d971fdc7c
isabelxklee <hi@kleetime.com>
2022-09-18 11:05:35 -0400

    imported recolata font

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import * as Styled from "./component-styles";
+import * as Global from "../styles";
 
 const Card = ({ algo }) => {
   return (
@@ -8,7 +9,7 @@ const Card = ({ algo }) => {
       <h3>
         <Link to={`algorithms/${algo.id}`}>{algo.title}</Link>
       </h3>
-      <p>{algo.description}</p>
+      <Global.P>{algo.description}</Global.P>
       <p>Difficulty: {algo.difficulty}</p>
       <p>Category: {algo.category}</p>
     </Styled.CardElement>
--- src/styles.js
+++ src/styles.js
@@ -1,9 +1,19 @@
 import styled, { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: "Recoleta";
+    src: url("./fonts/Recoleta-Regular.otf") format("otf");
+  }
+
   body {
+    font-family: "Recoleta", serif;
     color: #000;
     background: #fff;
-    font-family: "Helvetica", sans-serif;
   }
+`;
+
+export const P = styled.p`
+  margin: 0;
+  font-size: 16px;
 `;
