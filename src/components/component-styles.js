123cadc06bbc033777594f4d0a0944c7ff674eae
isabelxklee <hi@kleetime.com>
2022-09-18 10:40:00 -0400

    updated card style

--- src/components/component-styles.js
+++ src/components/component-styles.js
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 
 export const CardsContainer = styled.div`
+  display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 8px;
+
+  @media (max-width: 500px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export const CardElement = styled.div`
