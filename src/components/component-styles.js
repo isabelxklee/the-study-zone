84ebe74421c10382dba0e4d844347900832e0c77
isabelxklee <hi@kleetime.com>
2022-09-18 10:43:15 -0400

    updated cards container styling

--- src/components/component-styles.js
+++ src/components/component-styles.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export const CardsContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  gap: 8px;
+  gap: 16px;
+  padding: 120px;
 
   @media (max-width: 500px) {
     grid-template-columns: 1fr;
@@ -13,4 +14,7 @@ export const CardsContainer = styled.div`
 export const CardElement = styled.div`
   border-radius: 8px;
   border: 1px solid #000;
+  padding: 16px;
+  filter: drop-shadow(0 0 6px rgb(0, 0, 0, 0.2));
+  background: #fff;
 `;
