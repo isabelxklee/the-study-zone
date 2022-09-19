67692584b2acc990f8c84eefcee0400627b45f3f
isabelxklee <hi@kleetime.com>
2022-09-18 20:31:53 -0400

    replaced recoleta otf with woff

--- src/styles.js
+++ src/styles.js
@@ -1,17 +1,33 @@
 import styled, { createGlobalStyle } from "styled-components";
-import RecoletaRegular from "./fonts/Recoleta-Regular.otf";
+import RecoletaRegular from "./fonts/Recoleta-Regular.woff";
+import RecoletaBold from "./fonts/Recoleta-Bold.woff";
+import RingsideWideBold from "./fonts/Ringside-Wide-Bold.otf";
 
 export const GlobalStyle = createGlobalStyle`
-
   @font-face {
     font-family: "Recoleta";
     font-style: normal;
     font-weight: 400;
-    src: url(${RecoletaRegular}) format("otf");
+    src: url(${RecoletaRegular}) format("woff");
+  }
+
+  @font-face {
+    font-family: "Recoleta";
+    font-style: normal;
+    font-weight: 600;
+    src: url(${RecoletaBold}) format("woff");
+  }
+
+  @font-face {
+    font-family: "Ringside Wide";
+    font-style: normal;
+    font-weight: 600;
+    src: url(${RingsideWideBold}) format("otf");
   }
 
   body {
     font-family: "Recoleta", serif;
+    /* font-family: "Ringside Wide", sans-serif; */
     font-weight: 400;
     color: #000;
     background: #fff;
