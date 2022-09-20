0d06c379559040b94af7bad2b8c9f735c5ea1cda
isabelxklee <hi@kleetime.com>
2022-09-19 22:26:02 -0400

    updated dummy data

--- src/data.js
+++ src/data.js
@@ -15,4 +15,41 @@ export const data = [
     difficulty: "Hard",
     category: "String",
   },
+  {
+    id: 3,
+    title: "Trapping Rain Water",
+    description:
+      "Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.",
+    difficulty: "Hard",
+    category: "Array",
+  },
+  {
+    id: 4,
+    title: "Binary Tree Paths",
+    description:
+      "Given the root of a binary tree, return all root-to-leaf paths in any order. A leaf is a node with no children.",
+    difficulty: "Easy",
+    category: "Binary Tree",
+  },
 ];
+
+export const solutions = [
+  {
+    id: 1,
+    explanation: "Two pointer approach.",
+    timeComplexity: "O(n)",
+    spaceComplexity: "O(n)",
+    algorithmId: 1,
+  },
+];
+
+export const categories = [
+  "String",
+  "Array",
+  "Linked List",
+  "Binary Tree",
+  "Stack",
+  "Queue",
+];
+
+export const difficultyLevels = ["Easy", "Medium", "Hard"];
