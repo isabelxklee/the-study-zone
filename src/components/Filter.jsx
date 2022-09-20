a41eaab10dc854f7665f389de356b54e62373b4c
isabelxklee <hi@kleetime.com>
2022-09-19 22:44:45 -0400

    updated filter styling

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import * as Global from "../styles";
-import { categories } from "../data";
+import { categories, difficultyLevels } from "../data";
 
 const Filter = () => {
   return (
     <div>
       <Global.H3>Select a filter</Global.H3>
-      {categories
-        ? categories.map((category) => (
-            <Global.Tag key={category}>{category}</Global.Tag>
-          ))
-        : false}
+      <Global.TagsContainer>
+        {categories
+          ? categories.map((category) => (
+              <Global.Tag key={category}>{category}</Global.Tag>
+            ))
+          : false}
+        {difficultyLevels
+          ? difficultyLevels.map((level) => (
+              <Global.Tag primary key={level}>
+                {level}
+              </Global.Tag>
+            ))
+          : false}
+      </Global.TagsContainer>
     </div>
   );
 };
