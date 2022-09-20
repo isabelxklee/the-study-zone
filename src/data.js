a5f18ed8a48c86a271d256922923761069513005
isabelxklee <hi@kleetime.com>
2022-09-19 22:18:46 -0400

    created data file to store dummy data

---
+++ src/data.js
@@ -0,0 +1,18 @@
+export const data = [
+  {
+    id: 1,
+    title: "Two Sum",
+    description:
+      "Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.",
+    difficulty: "Easy",
+    category: "Array",
+  },
+  {
+    id: 2,
+    title: "Palindrome Pairs",
+    description:
+      "Given a list of unique words, return all the pairs of the distinct indices (i, j) in the given list, so that the concatenation of the two words words[i] + words[j] is a palindrome.",
+    difficulty: "Hard",
+    category: "String",
+  },
+];
