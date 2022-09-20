579a5990dadbde45d2a56bedfe148ccb28e0bddd
isabelxklee <hi@kleetime.com>
2022-09-19 22:43:36 -0400

    removed png references from manifest.json and updated filter logic

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import * as Global from "../styles";
+import { categories } from "../data";
 
 const Filter = () => {
-  return <Global.H3>Select a filter</Global.H3>;
+  return (
+    <div>
+      <Global.H3>Select a filter</Global.H3>
+      {categories
+        ? categories.map((category) => (
+            <Global.Tag key={category}>{category}</Global.Tag>
+          ))
+        : false}
+    </div>
+  );
 };
 
 export default Filter;
