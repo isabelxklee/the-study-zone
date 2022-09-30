dd37c7c12ed7aa1570ca61b2de14e67cba92c22c
isabelxklee <hi@kleetime.com>
2022-09-29 22:42:53 -0400

    made whole card clickable

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,12 +5,10 @@ import * as Global from "../styles";
 const Card = ({ algo, setSelectedTag }) => {
   return (
     <Styled.CardElement>
-      <Global.H3>
-        <Global.InternalLink to={`algorithms/${algo.id}`}>
-          {algo.name}
-        </Global.InternalLink>
-      </Global.H3>
-      <Global.P>{algo.description}</Global.P>
+      <Global.InternalLink to={`algorithms/${algo.id}`}>
+        <Global.H3>{algo.name}</Global.H3>
+        <Global.P>{algo.description}</Global.P>
+      </Global.InternalLink>
       <Global.TagsContainer>
         <Global.Tag
           $primary
