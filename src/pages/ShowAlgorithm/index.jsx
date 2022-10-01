78f8292b3af216cb13a36dfa4ad0fc86ccb67b13
isabelxklee <hi@kleetime.com>
2022-10-01 15:32:10 -0400

    installed v5 react router dom

--- src/pages/ShowAlgorithm/index.jsx
+++ src/pages/ShowAlgorithm/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams, useMatch } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { GlobalStyle } from "../../styles";
 
 const ShowAlgorithm = () => {
