41ebe57184451304d1eb37ccf5761fb95a147da8
isabelxklee <hi@kleetime.com>
2022-10-01 23:56:51 -0400

    created scroll to top component

--- src/pages/ShowAlgorithm/index.jsx
+++ src/pages/ShowAlgorithm/index.jsx
@@ -2,24 +2,26 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import { GlobalStyle } from "../../styles";
 import * as Global from "../../styles";
+import ScrollToTop from "../../components/ScrollToTop";
 
 const ShowAlgorithm = ({ algo }) => {
-  console.log(algo);
-
   return (
-    <Global.Wrapper>
-      <GlobalStyle />
-      <Global.H1>{algo.name}</Global.H1>
-      <Global.P>{algo.description}</Global.P>
-      <Global.TagsContainer>
-        <Global.Tag $primary $opacity={true}>
-          {algo.difficulty.name}
-        </Global.Tag>
-        <Global.Tag $opacity={true}>{algo.category.name}</Global.Tag>
-      </Global.TagsContainer>
-      <Global.H3>My Solution</Global.H3>
-      <ReactMarkdown>{algo.solution}</ReactMarkdown>
-    </Global.Wrapper>
+    <>
+      <ScrollToTop />
+      <Global.Wrapper>
+        <GlobalStyle />
+        <Global.H1>{algo.name}</Global.H1>
+        <Global.P>{algo.description}</Global.P>
+        <Global.TagsContainer>
+          <Global.Tag $primary $opacity={true}>
+            {algo.difficulty.name}
+          </Global.Tag>
+          <Global.Tag $opacity={true}>{algo.category.name}</Global.Tag>
+        </Global.TagsContainer>
+        <Global.H3>My Solution</Global.H3>
+        <ReactMarkdown>{algo.solution}</ReactMarkdown>
+      </Global.Wrapper>
+    </>
   );
 };
 
